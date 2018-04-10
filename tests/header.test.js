87e0ed018e5006642bee08b37c30789c8a77fc3d
Zach Baker <zach.r.baker1@gmail.com>
2018-04-10 15:40:54 -0700

    travis ci config

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,11 +1,9 @@
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
 const Page = require('./helpers/page');
 let page;
 
 beforeEach(async () => {
 	page = await Page.build();
-	await page.goto('localhost:3000');
+	await page.goto('http://localhost:3000');
 });
 
 afterEach(async () => {
@@ -14,7 +12,7 @@ afterEach(async () => {
 
 test('the header has the correct text', async () => {
 
-	const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+	const text = await page.getContentsOf('a.brand-logo');
 
 	expect(text).toEqual('Blogster');
 });
@@ -28,15 +26,9 @@ test('clicking login starts Oauth flow', async () => {
 })
 
 test('when signed in, shows logout button', async () => {
-	const user = await userFactory();
-	const { session, sig } = sessionFactory(user);
+	await page.login();
 
-	await page.setCookie({ name: 'session', value: session });
-	await page.setCookie({ name: 'session.sig', value: sig });
-	await page.goto('localhost:3000');
-	await page.waitFor('a[href="/auth/logout"]');
-
-	const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+	const text = await page.getContentsOf('a[href="/auth/logout"]');
 
 	expect(text).toEqual('Logout')
 
